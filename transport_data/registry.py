41f91970ac70b522738695c0f90ac265e5d59bcf
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-04-16 18:49:43 +0200

    Add registry.write() function

--- transport_data/registry.py
+++ transport_data/registry.py
@@ -2,6 +2,10 @@
 import subprocess
 
 import click
+import sdmx
+import sdmx.model.v21 as m
+
+from transport_data import CONFIG
 
 
 def _gh(*parts):
@@ -11,9 +15,45 @@ def _gh(*parts):
 
 def _git(*parts):
     """Invoke `git` in the :attr:`Config.tdc_registry_local` directory."""
-    from transport_data import CONFIG
-
     return subprocess.run(("git", "-C", str(CONFIG.tdc_registry_local)) + parts)
+
+
+def path_for(obj: m.MaintainableArtefact):
+    """Determine a path and filename for `obj`."""
+    if obj.urn is None:
+        obj.urn = sdmx.urn.make(obj)
+
+    return CONFIG.tdc_registry_local.joinpath(
+        obj.maintainer.id,
+        f"{obj.__class__.__name__}_{obj.maintainer.id}_{obj.id}_"
+        f"{obj.version.replace('.', '-')}",
+    ).with_suffix(".xml")
+
+
+def write(obj: m.MaintainableArtefact, force=False):
+    """Write `obj` into the registry as SDMX-ML.
+
+    The path and filename are determined by the object properties.
+    """
+    path = path_for(obj)
+
+    if path.exists() and not force:
+        raise RuntimeError(f"Will not overwrite {path} without force=True")
+
+    # Make the parent directory (but not multiple parents)
+    path.parent.mkdir(exist_ok=True)
+
+    with open(path, "wb") as f:
+        f.write(sdmx.to_xml(obj, pretty_print=True))
+
+    print(f"Wrote {path}")
+
+    # Add to git, but do not commit
+    _git("add", str(path.relative_to(CONFIG.tdc_registry_local)))
+    _git("status")
+
+
+# Command-line interface
 
 
 @click.group("registry", help=__doc__)
@@ -28,12 +68,4 @@ def clone():
     The registry is cloned into the directory specified by the `tdc_registry_local`
     config value. See `tdc config --help`.
     """
-    from transport_data import CONFIG
-
     _gh("repo", "clone", "transport-data/registry", str(CONFIG.tdc_registry_local))
-
-
-@main.command(hidden=True)
-def status():
-    """Example command using `git`."""
-    _git("status")
