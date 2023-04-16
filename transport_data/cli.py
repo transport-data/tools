4940e4b62e658868cec7ec32c91ccc2b5371ae3b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-04-16 12:22:10 +0200

    Add submodule commands/groups to top-level CLI

--- transport_data/cli.py
+++ transport_data/cli.py
@@ -1,6 +1,16 @@
+from importlib import import_module
+
 import click
 
 
-@click.command("tdc")
+@click.group("tdc")
 def main():
     """Transport Data Commons tools."""
+
+
+# Add subcommands from each module that defines them
+for name in ("proto",):
+    # Import a CLI submodule
+    module = import_module(f"transport_data.{name}.cli")
+    # Add the command named "main"
+    main.add_command(getattr(module, "main"))
