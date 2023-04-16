5e1340ccd71116ae0e909d7546496c58189a3567
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-04-16 20:01:03 +0200

    Add .util.click.common_params()

---
+++ transport_data/util/click.py
@@ -0,0 +1,8 @@
+import click
+
+PARAM = {"version": click.Option(["--version"])}
+
+
+def common_params(names: str) -> list:
+    """Return parameters from `PARAM`."""
+    return list(PARAM[k] for k in names.split())
