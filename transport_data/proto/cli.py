7c60ca2345d30729b47f3fe8e1c7bf5943f2e638
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-04-17 22:09:07 +0200

    Update "tdc proto" debugging command

--- transport_data/proto/cli.py
+++ transport_data/proto/cli.py
@@ -3,13 +3,19 @@ import click
 
 @click.command("proto")
 def main():
-    """TDC prototype."""
-    from transport_data import jrc
+    """TDC prototype debugging."""
+    from transport_data import adb, jrc
 
-    # commented: this fetches *all* data, and should not be done too often
-    # jrc.extract_all()
-    # commented: uncomment these statements to fetch and unpack just one data file
-    path = jrc.fetch("AT")
-    jrc.extract(path)
+    # # commented: this fetches *all* data, and should not be done too often
+    # # jrc.extract_all()
+    #
+    # # commented: uncomment these statements to fetch and unpack just one data file
+    # path = jrc.fetch("AT")
+    # jrc.extract(path)
 
     print(jrc.read("AT"))
+
+    # # commented: uncomment this statement to fetch ADB ATO data files
+    # adb.get()
+
+    adb.convert_all()
