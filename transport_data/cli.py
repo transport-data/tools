3e508f5d94381bd019433a0aece1937898140cfd
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-04-21 22:55:38 +0200

    Add .estat as a demo of direct SDMX access

--- transport_data/cli.py
+++ transport_data/cli.py
@@ -8,9 +8,18 @@ def main():
     """Transport Data Commons tools."""
 
 
-# Add subcommands from each module that defines them
-for name in ("config", "org.cli", "proto.cli", "registry"):
-    # Import a CLI submodule
+#: List of (sub)modules that define CLI (sub)commands. Each should contain a
+#: @click.command() named "main".
+MODULES_WITH_CLI = [
+    "config",
+    "estat",
+    "org.cli",
+    "proto.cli",
+    "registry",
+]
+
+
+# Add commands from each module that defines them
+for name in MODULES_WITH_CLI:
     module = import_module(f"transport_data.{name}")
-    # Add the command named "main"
     main.add_command(getattr(module, "main"))
---
+++ transport_data/estat/__init__.py
@@ -0,0 +1,67 @@
+"""Eurostat (ESTAT) data provider.
+
+Eurostat is used as to demonstrate interaction with a data provider that offers:
+
+- A web service that conforms to the SDMX REST API standard.
+- Data and metadata in the SDMX-ML formatl through this API.
+
+In this case, TDC tools can retrieve the structure information and data directly, and no
+provider-specific conversion code is needed.
+"""
+import click
+import sdmx
+
+from transport_data import registry
+
+# General functions
+
+
+def list_flows():
+    """Return a list of data flows."""
+    # TODO use the API to retrieve a list of transport-related data flows
+    return ["TRAN_HV_MS_FRMOD"]
+
+
+def get(dataflow_id: str):
+    """Retrieve the ESTAT structure and data for the data flow with ID `dataflow_id`."""
+    client = sdmx.Client("ESTAT")
+
+    # Retrieve structural information as an sdmx.StructureMessage
+    sm = client.dataflow(dataflow_id)
+
+    # Write each of the structure objects received to a separate file
+    for kind in ("codelist", "concept_scheme", "dataflow", "structure"):
+        for obj in getattr(sm, kind).values():
+            registry.write(obj, annotate=False, force=True)
+
+    # Retrieve the data itself
+    dm = client.data(dataflow_id)
+
+    # Extract a single data set from the data message
+    ds = dm.data[0]
+    # Assign an explicit reference to the data set. The ESTAT response comes back with
+    # an "anonymous" dataflow.
+    ds.described_by = sm.dataflow[dataflow_id]
+
+    # Write to file
+    path = registry.write(ds)
+    print(f"Retrieved {path}")
+    print("          and associated structures")
+
+    return path
+
+
+# Command-line interface
+
+
+@click.group("estat", help=__doc__)
+def main():
+    pass
+
+
+@main.command("fetch")
+def fetch_cmd():
+    """Retrieve data."""
+    for dataflow_id in list_flows():
+        print(f"Retrieve data flow {dataflow_id!r} and structure")
+        get(dataflow_id)
