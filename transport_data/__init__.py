7b45c02c6ada157d9646b6ec719947668ace4bd3
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-04-23 11:40:49 +0200

    Add basic logging configuration

--- transport_data/__init__.py
+++ transport_data/__init__.py
@@ -1,3 +1,9 @@
+import logging
+import sys
+
 from .config import Config
+
+logging.basicConfig(level=logging.INFO, handlers=logging.StreamHandler(sys.stdout))
+logging.getLogger(__name__).setLevel(logging.INFO)
 
 CONFIG = Config.read()
