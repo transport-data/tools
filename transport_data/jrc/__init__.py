ae99964564bf11d60a5a2f1560257a35fc8c5e1f
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-05-11 13:38:26 +0200

    Expand .jrc.read()

--- transport_data/jrc/__init__.py
+++ transport_data/jrc/__init__.py
@@ -9,9 +9,12 @@
 
 """
 import re
+from functools import partial
+from operator import add
 from pathlib import Path
 from zipfile import ZipFile
 
+import numpy as np
 import pandas as pd
 import requests
 import sdmx.model.v21 as m
@@ -112,6 +115,143 @@ def extract_all():
         extract(p)
 
 
+def iter_blocks(path: Path, geo: str):
+    ef = pd.ExcelFile(path)
+
+    skip_sheets = ["cover", "index"]
+    parse_args = dict(usecols="A:Q", header=None)
+    columns = pd.Index([])
+    for sheet_name in filter(lambda n: n not in skip_sheets, ef.sheet_names):
+        common = dict(GEO=geo)
+
+        # Extract a hint for MODE from the sheet name
+        if match := re.match("Tr(Avia|Navi|Rail|Road)_...", sheet_name):
+            value = match.group(1).upper()
+            common.update(MODE={"AVIA": "AIR", "NAVI": "WATER"}.get(value, value))
+
+        df = ef.parse(sheet_name, **parse_args)
+
+        # Identify blank rows that separate blocks of data
+        breaks = df.index[df.isna().all(axis=1)].to_list()
+
+        # Iterate over blocks
+        for start, end in zip(map(partial(add, 1), [-1] + breaks), breaks):
+            data = df.loc[start : end - 1, :]
+            if start == 0:
+                assert 1 == data.shape[0]
+                columns = pd.Index(["INFO"] + data.iloc[0, 1:].astype(int).to_list())
+                continue
+            elif data.iloc[0, 0] == "Indicators":
+                assert 1 == data.shape[0] and data.iloc[0, 1:].isna().all()
+                continue
+
+            yield data.pipe(_set_axis, columns).assign(**common).reset_index(drop=True)
+
+
+def _set_axis(df: pd.DataFrame, columns: pd.Index) -> pd.DataFrame:
+    melt_kw = dict(id_vars="INFO", value_name="OBS_VALUE")
+    if df.iloc[0, 0] is np.nan:
+        # Handle a different structure used in TrRoad_tech
+        # First row contains the single TIME_PERIOD label applying to all data
+        tp = df.iloc[0, :].dropna().item()
+        # Construct a different set of columns from the *second* row of `df`, and only
+        # carry through the third row et seq. that contain actual data
+        return (
+            df.iloc[2:, :]
+            .set_axis(pd.Index(["INFO"] + df.iloc[1, 1:].to_list()), axis=1)
+            .melt(**melt_kw, var_name="YEAR_REG")
+            .assign(TIME_PERIOD=tp)
+        )
+    else:
+        return df.set_axis(columns, axis=1).melt(**melt_kw, var_name="TIME_PERIOD")
+
+
+def _match_extract(df: pd.DataFrame, expr) -> pd.DataFrame:
+    expr_ = re.compile(expr)
+    cols = list(expr_.groupindex.keys())
+
+    matches = df["INFO"].str.fullmatch(expr_)
+    result = pd.concat([df, df["INFO"].str.extract(expr_)[cols].ffill()], axis=1)
+    result.loc[matches, "INFO"] = "ALL"
+    return result
+
+
+def _fill_unit_measure(df: pd.DataFrame, measure: str) -> pd.DataFrame:
+    """Fill in the UNIT_MEASURE column of `df`."""
+    # Identify the MODE, if any
+    mode = df.get("MODE", pd.Series([None])).unique().item()
+    return df.assign(
+        UNIT_MEASURE=lambda df: df[["U0", "U1"]]
+        .fillna(UNIT_MEASURE.get((mode, measure), ""))
+        .bfill(axis=1)["U0"]
+    ).drop(["U0", "U1"], axis=1)
+
+
+#: Mapping from (MODE, MEASURE) to UNIT_MEASURE attribute value, where these are not
+#: specified in the data.
+UNIT_MEASURE = {
+    # Both TrRoad_act and TrRoad_tech
+    ("ROAD", "New vehicle-registrations"): "vehicle",
+    # TrRoad_tech
+    ("ROAD", "Age structure in 2015"): "vehicle",
+    # TrRail_act
+    ("RAIL", "Occupancy / utilisation"): "percent",
+    # TrAvia_act
+    ("AIR", "Number of flights"): "1",
+    ("AIR", "Stock of aircrafts - total"): "vehicle",
+    ("AIR", "Stock of aircrafts - in use"): "vehicle",
+    ("AIR", "New aircrafts"): "vehicle",
+    ("AIR", "Flights per year by airplance"): "1",
+    # TrAvia_png
+    ("AIR", "Number of seats available"): "1",
+    ("AIR", "Seats available per flight"): "1",
+}
+
+UNPACK = {
+    "ALL": dict(MODE="ALL"),
+    "Road transport": dict(MODE="ROAD", VEHICLE_TYPE="ALL"),
+    "Powered 2-wheelers": dict(MODE="ROAD", VEHICLE_TYPE="2W"),
+    "Passenger cars": dict(MODE="ROAD", VEHICLE_TYPE="LDV"),
+    "Gasoline engine": dict(MODE="ROAD", VEHICLE_TYPE="LDV", POWERTRAIN="GAS"),
+    "Diesel oil engine": dict(MODE="ROAD", VEHICLE_TYPE="LDV", POWERTRAIN="DIES"),
+    "LPG engine": dict(MODE="ROAD", VEHICLE_TYPE="LDV", POWERTRAIN="LPG"),
+    "Natural gas engine": dict(MODE="ROAD", VEHICLE_TYPE="LDV", POWERTRAIN="NG"),
+    "Plug-in hybrid electric": dict(MODE="ROAD", VEHICLE_TYPE="LDV", POWERTRAIN="PHEV"),
+    "Battery electric vehicles": dict(
+        MODE="ROAD", VEHICLE_TYPE="LDV", POWERTRAIN="BEV"
+    ),
+    "Motor coaches, buses and trolley buses": dict(MODE="ROAD", VEHICLE_TYPE="BUS"),
+    "Rail, metro and tram": dict(MODE="RAIL", VEHICLE_TYPE="ALL"),
+    "Metro and tram, urban light rail": dict(MODE="RAIL", VEHICLE_TYPE="L"),
+    "Conventional passenger trains": dict(MODE="RAIL", VEHICLE_TYPE="H"),
+    "High speed passenger trains": dict(MODE="RAIL", VEHICLE_TYPE="HS"),
+    "Aviation": dict(MODE="AIR", SEGMENT="ALL"),
+    "Domestic": dict(MODE="AIR", SEGMENT="DOM"),
+    "International - Intra-EU": dict(MODE="AIR", SEGMENT="IN_EU"),
+    "International - Extra-EU": dict(MODE="AIR", SEGMENT="EX_EU"),
+    # Freight
+    "Light duty vehicles": dict(MODE="ROAD", VEHICLE_TYPE="LDV"),
+    "Heavy duty vehicles": dict(MODE="ROAD", VEHICLE_TYPE="HDV"),
+    "Rail transport": dict(MODE="RAIL", VEHICLE_TYPE="ALL"),
+    "Domestic and International - Intra-EU": dict(MODE="RAIL", SEGMENT="DOM_IN_EU"),
+    "Coastal shipping and inland waterways": dict(MODE="WATER", SEGMENT="ALL"),
+    "Domestic coastal shipping": dict(MODE="WATER", SEGMENT="DOM"),
+    "Inland waterways": dict(MODE="WATER", SEGMENT="IWW"),
+}
+
+
+def _unpack_info(df: pd.DataFrame) -> pd.DataFrame:
+    print(
+        df["INFO"]
+        .drop_duplicates()
+        .apply(lambda v: pd.Series(UNPACK[v]))
+        .fillna("_Z")
+        .to_string()
+    )
+
+    raise NotImplementedError
+
+
 def read(geo=None):
     """Read data from a single file.
 
@@ -123,80 +263,78 @@ def read(geo=None):
     """
     path = path_for(geo).with_name(f"JRC-IDEES-2015_Transport_{geo}.xlsx")
 
-    ef = pd.ExcelFile(path)
+    # Metadata across all blocks
+    ALL_INFO = set()
+    CS_MEASURE = m.ConceptScheme(id="MEASURE")
+    CL_UNIT_MEASURE = m.Codelist(id="UNIT_MEASURE")
 
-    for sheet_name, usecols in (("Transport", "A:Q"),):
-        df = ef.parse(sheet_name, usecols=usecols)
+    # Iterate over blocks of data
+    for i, block in enumerate(iter_blocks(path, geo)):
+        # Identify the measure (INFO column on first row of the block)
+        measure_unit = block.loc[0, "INFO"]
 
-        # TODO move the following in to convert()
+        # Unpack a string that combines MEASURE and UNIT_MEASURE
+        match = re.fullmatch(r"(?P<measure>[^\(]+) \((?P<unit>.*)\)\*?", measure_unit)
+        try:
+            measure, unit = match.group("measure", "unit")
+        except AttributeError:
+            measure, unit = measure_unit, None
 
-        # - Rename the label column
-        # - Melt into long format
-        # - Assign GEO and empty MEASURE column
-        idx = ["INFO"] + df.columns.tolist()[1:]
-        df = (
-            df.set_axis(idx, axis=1)
-            .melt(id_vars="INFO", var_name="TIME_PERIOD", value_name="OBS_VALUE")
-            .assign(GEO=geo, MEASURE="", UNIT_MEASURE="", SERVICE="")
+        # - Assign `unit` from above as U0, one candidate from UNIT_MEASURE.
+        # - Replace the embedded measure/unit expression with "ALL transport".
+        # - Unpack and fill forward SERVICE values from "Passenger transport" or
+        #   "Freight transport" entries in the INFO column. Also unpack U1.
+        # - Choose the right
+        # - If UNIT_MEASURE is still missing, fill from explicit values above.
+        # - Drop rows with missing values in "OBS_VALUE"
+        block = (
+            block.assign(U0=unit)
+            .replace({"INFO": {measure_unit: "ALL transport"}})
+            .pipe(
+                _match_extract,
+                r"(?P<SERVICE>ALL|Freight|Passenger) transport( +\((?P<U1>.*)\))?",
+            )
+            .pipe(_fill_unit_measure, measure)
+            .dropna(subset="OBS_VALUE", ignore_index=True)
+            .pipe(_unpack_info)
         )
 
-        # Identify breaks between blocks of data for the same measure
-        breaks = df.isna().query("INFO and OBS_VALUE").index.to_list()
-        # Iterate over paired indices for the start and end of each block
-        for start, end in zip(map(lambda n: n + 1, breaks), breaks[1:] + [len(df) + 1]):
-            # Identify the measure (INFO column on first row of the block)
-            measure = df.loc[start, "INFO"]
+        # Update the structure information
 
-            # Handle a string that combines MEASURE and UNIT_MEASURE
-            if match := re.fullmatch(r"(?P<measure>[^\(]+) \((?P<unit>.*)\)", measure):
-                measure = match.group("measure")
-                unit = match.group("unit")
-            else:
-                unit = ""
+        # Units of measure
+        for unit in block["UNIT_MEASURE"].unique():
+            try:
+                CL_UNIT_MEASURE.append(m.Code(id=unit, name=unit))
+            except ValueError:
+                pass  # Already exists
 
-            # Assign values within this block
-            df.loc[start:end, "MEASURE"] = measure
-            df.loc[start:end, "UNIT_MEASURE"] = unit
+        # The measure concept
+        measure_concept = m.Concept(id=str(i), name=measure)
+        CS_MEASURE.append(measure_concept)
 
-            # Replace the embedded value in the INFO column with "ALL"
-            df.loc[start, ["INFO", "SERVICE"]] = "ALL"
+        # Remaining labels appearing in the "INFO" dimension
+        ALL_INFO.update(block["INFO"].unique())
 
-        # Drop null values
-        df = df.dropna(subset=["INFO", "OBS_VALUE"]).reset_index(drop=True)
+        # First two rows, transposed
+        print("\n", repr(measure_concept))
+        print(block.head(2).transpose().to_string())
 
-        # Similar process between passenger and freight
-        expr = r"(ALL|(?P<service>Freight|Passenger) transport( \((?P<unit>.*)\))?)"
-        breaks = df[df["INFO"].str.fullmatch(expr)].index.to_list()
-        for start, end in zip(breaks, map(lambda n: n - 1, breaks[1:] + [len(df) + 1])):
-            # Identify the service (INFO column on first row of the block)
-            service = df.loc[start, "INFO"]
-            if service == "ALL":
-                continue
+        # print("\n", repr(measure_concept))
+        # print(block.to_string())
 
-            existing = list(df.loc[start:end, "UNIT_MEASURE"].unique())
+    print(
+        "---",
+        f"{len(ALL_INFO)} distinct INFO labels",
+        "\n".join(sorted(ALL_INFO)),
+        "---",
+        sep="\n",
+    )
+    print("---", repr(CL_UNIT_MEASURE), CL_UNIT_MEASURE.items, sep="\n")
 
-            # Handle a string that combines SERVICE and UNIT_MEASURE
-            if match := re.fullmatch(expr, service):
-                service = match.group("service")
-                unit = match.group("unit") or existing[0]
-            else:
-                unit = ""
+    # TODO extract MODE and VEHICLE from INFO
+    # TODO create SDMX DSD, Dataflow, and DataSets from the resulting pd.DataFrame
 
-            # Check for any conflict
-            if unit:
-                assert set(existing) < {"", unit}, f"{existing = } vs. {unit = }"
-
-            # Assign values within this block
-            df.loc[start:end, "SERVICE"] = service
-            df.loc[start:end, "UNIT_MEASURE"] = unit
-
-            # Replace the embedded value in the INFO column with "ALL"
-            df.loc[start, "INFO"] = "ALL"
-
-        # TODO extract MODE and VEHICLE from INFO
-        # TODO create SDMX DSD, Dataflow, and DataSets from the resulting pd.DataFrame
-
-        return df
+    # raise NotImplementedError
 
 
 def convert():
