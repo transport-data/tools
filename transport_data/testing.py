0bc8bb266d405577dc439eab81f46db0addca56c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-12-21 12:06:43 -0500

    Write sdmx_structures fixture data to file

--- transport_data/testing.py
+++ transport_data/testing.py
@@ -2,6 +2,8 @@ import pytest
 import sdmx.message
 import sdmx.model.v21 as m
 
+import transport_data
+
 
 @pytest.fixture(scope="session")
 def sdmx_structures() -> sdmx.message.StructureMessage:
@@ -10,18 +12,18 @@ def sdmx_structures() -> sdmx.message.StructureMessage:
 
     a = m.Agency(id="TEST")
 
-    cs = m.ConceptScheme(id="TEST")
+    cs = m.ConceptScheme(id="TEST", maintainer=a)
     cs.append(m.Concept(id="MASS", name="Mass of fruit"))
     cs.append(m.Concept(id="PICKED", name="Number of fruits picked"))
     cs.append(m.Concept(id="COLOUR", name="Colour of fruit"))
     cs.append(m.Concept(id="FRUIT", name="Type of fruit"))
 
-    cl = m.Codelist(id="COLOUR")
+    cl = m.Codelist(id="COLOUR", maintainer=a)
     cl.extend([m.Code(id=c, name=c.title()) for c in "GREEN ORANGE RED YELLOW".split()])
     cl.append(m.Code(id="_T", name="Total"))
     sm.add(cl)
 
-    cl = m.Codelist(id="FRUIT")
+    cl = m.Codelist(id="FRUIT", maintainer=a)
     cl.extend(
         [m.Code(id=c, name=c.title()) for c in "APPLE BANANA GRAPE LEMON".split()]
     )
@@ -44,5 +46,7 @@ def sdmx_structures() -> sdmx.message.StructureMessage:
             for d in dims
         )
         sm.add(dsd)
+
+    transport_data.STORE.write(sm)
 
     return sm
