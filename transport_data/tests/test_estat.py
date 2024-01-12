9f1ddd77e1fe97a6ee5ad7e21b13505ae8d4ca9c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-01-12 16:37:28 +0100

    Mark test_estat.test_get() as flaky

--- transport_data/tests/test_estat.py
+++ transport_data/tests/test_estat.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from transport_data.estat import get, list_flows
@@ -7,6 +9,7 @@ def test_list_flows():
     list_flows()
 
 
+@pytest.mark.flaky(condition="GITHUB_ACTIONS" in os.environ, reruns=5)
 @pytest.mark.parametrize("df_id", list_flows())
 def test_get(df_id):
     get(df_id)
