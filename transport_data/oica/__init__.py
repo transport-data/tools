da744d0e7f0a67c805512c0435580ecb06d089d4
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-02-05 19:19:26 +0100

    Identify OICA data by measure/data flow ID
    Sort functions in module.

--- transport_data/oica/__init__.py
+++ transport_data/oica/__init__.py
@@ -1,22 +1,19 @@
 """International Organization of Motor Vehicle Manufacturers (OICA) provider.
 
-This module handles data from the OICA website.
+This module handles data from the OICA website. "Handle" includes:
 
-"Handle" includes:
-
-- Fetch
-- Extract
+- Fetch.
 - Parse the native spreadsheet layout.
 - Convert to SDMX.
-
 """
 import json
 import logging
+import re
 from functools import lru_cache, partial
 from itertools import count, product
 from operator import itemgetter
 from pathlib import Path
-from typing import TYPE_CHECKING, Dict, Tuple
+from typing import TYPE_CHECKING, Dict, Iterator, Tuple
 
 import pandas as pd
 
@@ -37,6 +34,128 @@ REGISTRY_FILE = Path(__file__).parent.joinpath("registry.json")
 # Read the registry file
 with open(REGISTRY_FILE) as f:
     POOCH = Pooch(module=__name__, base_url=BASE_URL, registry=json.load(f))
+
+
+def convert(measure: str):
+    """Convert OICA stock (vehicle in use) spreadsheets to SDMX."""
+    # Retrieve the DFD
+    dfd, dsd = get_structures(measure)
+
+    if "PROD" in dfd.id or "SALES" in dfd.id:
+        raise NotImplementedError(f"Convert OICA data files for {dfd}")
+
+    # Convert all fetchable/fetched files related to the DFD
+    [_convert_single_file(path) for path in filenames_for_dfd(dfd)]
+
+
+def _convert_tp(time_period: str, units: str, vehicle_type: str):
+    """Identify values for 4 concepts using the TIME_PERIOD."""
+    if time_period in (2015.0, 2020.0):
+        return pd.Series(
+            dict(
+                MEASURE="STOCK",
+                UNIT_MEASURE=units,
+                VEHICLE_TYPE=vehicle_type,
+                TIME_PERIOD=str(int(time_period)),
+            )
+        )
+    elif time_period == "Average Annual Growth Rate\n 2015-2020":
+        return pd.Series(
+            dict(
+                MEASURE="STOCK_AAGR",
+                UNIT_MEASURE="1",
+                VEHICLE_TYPE=vehicle_type,
+                TIME_PERIOD="2015–2020",
+            )
+        )
+    elif time_period == "Number of vehicles per 1000 inhabitants":
+        return pd.Series(
+            dict(
+                MEASURE="STOCK_CAP",
+                UNIT_MEASURE="0.001",
+                vehicle_type=vehicle_type,
+                TIME_PERIOD="_X",
+            )
+        )
+    else:
+        raise ValueError(time_period)
+
+
+def _convert_single_file(path: Path):
+    """Convert single OICA stock spreadsheet to SDMX."""
+    from sdmx.model.v21 import DataSet
+
+    from transport_data import STORE
+    from transport_data.util.sdmx import make_obs
+
+    # - Read data
+    # - Drop entirely empty rows.
+    # - Drop entirely empty columns.
+    df = (
+        pd.read_excel(path)
+        .dropna(axis=0, how="all", ignore_index=True)  # type: ignore [call-overload]
+        .dropna(axis=1, how="all")
+    )
+
+    # Confirm vehicle type and measure vs. contents of the title cell
+    pat = re.compile(r"(PC|CV|TOTAL) WORLD VEHICLES IN USE +\(in thousand units\)")
+
+    if match := pat.fullmatch(df.iloc[0, 0]):
+        units = "kvehicle"
+        vehicle_type = match.group(1)
+    else:
+        raise ValueError(f"Unrecognized table header: {df.iloc[0, 0]!r}")
+
+    # - Drop title cell.
+    # - Set index.
+    # - Rename REGIONS/COUNTRIES to GEO.
+    # - Melt to long format.
+    # - Replace float with string year values.
+    df = (
+        df.iloc[2:, :]
+        .set_axis(df.iloc[1, :], axis=1)
+        .rename(columns={"REGIONS/COUNTRIES": "GEO"})
+        .melt(id_vars="GEO", var_name="TIME_PERIOD", value_name="OBS_VALUE")
+    )
+
+    # Prepare a GEO codelist and map using the "GEO" column
+    cl_geo, geo_map = _geo_codelist(df["GEO"], maintainer=get_agency(), version="0.1")
+    # Store `cl_geo`
+    STORE.write(cl_geo)
+
+    # Transform data
+    # - Replace GEO values with codes from `cl_geo`.
+    # - Replace TIME_PERIOD values with new TIME_PERIOD, MEASURE, UNIT_MEASURE,
+    #   VEHICLE_TYPE.
+    # - Preserve OBS_VALUE.
+    df = pd.concat(
+        [
+            df["GEO"].replace(geo_map),
+            df["TIME_PERIOD"].apply(
+                _convert_tp, units=units, vehicle_type=vehicle_type
+            ),
+            df["OBS_VALUE"],
+        ],
+        axis=1,
+    )
+
+    # Group data by MEASURE ~ dataflow
+    for m, group_df in df.groupby("MEASURE"):
+        # Create structures for this measure
+        # TODO Retrieve possibly-cached or -stored structures
+        dfd, dsd = get_structures(m)
+
+        # Create a data set
+        ds = DataSet(described_by=dfd, structured_by=dsd)
+
+        # Convert rows of `group_df` to observations
+        ds.add_obs(
+            map(partial(make_obs, dsd=dsd), map(itemgetter(1), group_df.iterrows()))
+        )
+
+        # Write the data set to file
+        # TODO Merge with with other data for the same flow
+        STORE.write(ds)
 
 
 @lru_cache
@@ -118,6 +237,60 @@ def _geo_codelist(
     return cl, id_for_name
 
 
+def fetch(dry_run: bool = False):
+    """Fetch all known OICA data files."""
+    if dry_run:
+        for f in POOCH.registry:
+            print(f"Valid url for GEO={f}: {POOCH.is_available(f)}")
+        return
+
+    return [POOCH.fetch(f) for f in POOCH.registry]
+
+
+def filenames_for_dfd(
+    dfd: "sdmx.model.v21.DataflowDefinition", fetch: bool = True
+) -> Iterator[Path]:
+    """Yield OICA's file names for individual files in `dfd`.
+
+    Parameters
+    ----------
+    fetch : bool, optional
+        If :obj:`.True` (the default), yield only files that are fetched and available
+        locally. Otherwise, yield all possible names conforming to a known pattern.
+    """
+    if "PROD" in dfd.id:
+        pattern = "{}-{}.xlsx"
+        values = (
+            {
+                "By-country-region",
+                "Passenger-Cars",
+                "Light-Commercial-Vehicles",
+                "Heavy-Trucks",
+                "Buses-and-Coaches",
+            },
+            range(2017, 2024),
+        )
+    elif "SALES" in dfd.id:
+        pattern = "{}_sales_{}.xlsx"
+        values = {"cv", "pc", "total"}, range(2017, 2024)
+    elif "STOCK" in dfd.id:
+        pattern = "{}-World-vehicles-in-use-{}.xlsx"
+        values = {"CV", "PC", "Total"}, range(2017, 2024)
+    else:
+        raise NotImplementedError(f"Construct OICA file names for {dfd}")
+
+    # Generate a cartesian product of the iterables in `values`
+    for name_parts in product(*values):
+        # Format a file name
+        name = pattern.format(*name_parts)
+
+        try:
+            # Try to fetch if `fetch` is True; else return every name
+            yield Path(POOCH.fetch(name) if fetch else name)
+        except ValueError:
+            pass
+
+
 def get_structures(
     measure: str,
 ) -> Tuple[
@@ -157,117 +330,6 @@ def get_structures(
     return dfd, dsd
 
 
-def convert():
-    """Convert OICA stock (vehicle in use) spreadsheets to SDMX."""
-    from sdmx.model.v21 import DataSet
-
-    from transport_data import STORE
-    from transport_data.util.sdmx import make_obs
-
-    # Select just one spreadsheet
-    paths = fetch()
-    for p in paths:
-        if "PC-World" in p:
-            path = Path(p)
-            break
-
-    # - Read data
-    # - Drop entirely empty rows.
-    # - Drop entirely empty columns.
-    df = (
-        pd.read_excel(path)
-        .dropna(how="all", axis=0, ignore_index=True)
-        .dropna(how="all", axis=1)
-    )
-
-    # Confirm vehicle type and measure vs. contents of the title cell
-    assert "PC WORLD VEHICLES IN USE  (in thousand units)" == df.iloc[0, 0]
-    units = "kvehicle"
-    vehicle_type = "PC"
-
-    # - Drop title cell.
-    # - Set index.
-    # - Rename REGIONS/COUNTRIES to GEO.
-    # - Melt to long format.
-    # - Replace float with string year values.
-    df = (
-        df.iloc[2:, :]
-        .set_axis(df.iloc[1, :], axis=1)
-        .rename(columns={"REGIONS/COUNTRIES": "GEO"})
-        .melt(id_vars="GEO", var_name="TIME_PERIOD", value_name="OBS_VALUE")
-    )
-
-    def _convert_tp(time_period):
-        """Identify values for 4 concepts using the TIME_PERIOD."""
-        if time_period in (2015.0, 2020.0):
-            return pd.Series(
-                dict(
-                    MEASURE="STOCK",
-                    UNIT_MEASURE=units,
-                    VEHICLE_TYPE=vehicle_type,
-                    TIME_PERIOD=str(int(time_period)),
-                )
-            )
-        elif time_period == "Average Annual Growth Rate\n 2015-2020":
-            return pd.Series(
-                dict(
-                    MEASURE="AAGR_STOCK",
-                    UNIT_MEASURE="1",
-                    VEHICLE_TYPE=vehicle_type,
-                    TIME_PERIOD="2015–2020",
-                )
-            )
-        else:
-            raise ValueError(time_period)
-
-    # Prepare a GEO codelist and map using the "GEO" column
-    cl_geo, geo_map = _geo_codelist(df["GEO"], maintainer=get_agency(), version="0.1")
-    # Store `cl_geo`
-    STORE.write(cl_geo)
-
-    # Transform data
-    # - Replace GEO values with codes from `cl_geo`.
-    # - Replace TIME_PERIOD values with new TIME_PERIOD, MEASURE, UNIT_MEASURE,
-    #   VEHICLE_TYPE.
-    # - Preserve OBS_VALUE.
-    df = pd.concat(
-        [
-            df["GEO"].replace(geo_map),
-            df["TIME_PERIOD"].apply(_convert_tp),
-            df["OBS_VALUE"],
-        ],
-        axis=1,
-    )
-
-    # Group data by MEASURE ~ dataflow
-    for m, group_df in df.groupby("MEASURE"):
-        # Create structures for this measure
-        # TODO Retrieve possibly-cached or -stored structures
-        dfd, dsd = get_structures(m)
-
-        # Create a data set
-        ds = DataSet(described_by=dsd)
-
-        # Convert rows of `group_df` to observations
-        ds.add_obs(
-            map(partial(make_obs, dsd=dsd), map(itemgetter(1), group_df.iterrows()))
-        )
-
-        # Write the data set to file
-        # TODO Merge with with other data for the same flow
-        STORE.write(ds)
-
-
-def fetch(dry_run: bool = False):
-    """Fetch all known OICA data files."""
-    if dry_run:
-        for f in POOCH.registry:
-            print(f"Valid url for GEO={f}: {POOCH.is_available(f)}")
-        return
-
-    return [POOCH.fetch(f) for f in POOCH.registry]
-
-
 def update_registry():
     """Update the registry.
 
@@ -276,42 +338,19 @@ def update_registry():
     """
     from pooch import file_hash
 
-    for pattern, values in (
-        # Sales statistics
-        ("{}_sales_{}.xlsx", ({"cv", "pc", "total"}, range(2017, 2024))),
-        # Vehicles in use
-        (
-            "{}-World-vehicles-in-use-{}.xlsx",
-            ({"CV", "PC", "Total"}, range(2017, 2024)),
-        ),
-        # Production data
-        (
-            "{}-{}.xlsx",
-            (
-                {
-                    "By-country-region",
-                    "Passenger-Cars",
-                    "Light-Commercial-Vehicles",
-                    "Heavy-Trucks",
-                    "Buses-and-Coaches",
-                },
-                range(2017, 2024),
-            ),
-        ),
-    ):
-        for sub in product(*values):
-            url_part = pattern.format(*sub)
-            existing_hash = POOCH.registry.setdefault(url_part, None)
+    for dfd in map(get_structures, ["PROD", "SALES", "STOCK"]):
+        for filename in filenames_for_dfd(dfd, fetch=False):
+            existing_hash = POOCH.registry.setdefault(filename, None)
 
-            if not POOCH.is_available(url_part):
+            if not POOCH.is_available(filename):
                 # File doesn't exist on the remote
-                POOCH.registry.pop(url_part)
+                POOCH.registry.pop(filename)
                 continue
 
             if existing_hash is None:
                 # Download the file to add its hash
-                path = POOCH.fetch(url_part)
-                POOCH.registry[url_part] = file_hash(path)
+                path = POOCH.fetch(filename)
+                POOCH.registry[filename] = file_hash(path)
 
     with open(REGISTRY_FILE, "w") as f:
         json.dump(POOCH.registry, f)
--- transport_data/tests/test_oica.py
+++ transport_data/tests/test_oica.py
@@ -1,8 +1,19 @@
+import pytest
+
 from transport_data.oica import convert, update_registry
 
 
-def test_convert():
-    convert()
+@pytest.mark.parametrize(
+    "measure",
+    (
+        pytest.param("PROD", marks=pytest.mark.xfail(raises=NotImplementedError)),
+        pytest.param("SALES", marks=pytest.mark.xfail(raises=NotImplementedError)),
+        "STOCK",
+        "STOCK_AAGR",
+    ),
+)
+def test_convert(measure):
+    convert(measure)
 
 
 def test_update_registry():
