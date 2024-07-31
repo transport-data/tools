343218cc00fda3ba53d53ac31b2d948642ff1f8b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-07-31 15:52:11 +0200

    Add IPCC 2006 concept scheme

--- transport_data/ipcc/structure.py
+++ transport_data/ipcc/structure.py
@@ -1,3 +1,5 @@
+"""IPCC structural metadata."""
+
 from functools import lru_cache
 from typing import TYPE_CHECKING
 
@@ -18,7 +20,16 @@ def get_agency() -> "sdmx.model.common.Agency":
 
 
 def gen_cl_T311(**kwargs) -> "sdmx.model.Common.Codelist":
-    """Generate a code list from the GNGGI, Volume 2, Table 3.1.1."""
+    """Generate a code list from the GNGGI, Volume 2, Table 3.1.1.
+
+    The generated code list's URN ends with ``Codelist=TDCI:CL_IPCC_2006_V2_T3.1.1(…)``.
+
+    .. todo:: Expand to include 'Explanation' text from the table as descriptions for
+       codes.
+
+    .. todo:: Include internationalized texts (names, descriptions) from the Arabic,
+       Chinese, French, Russian, and/or Spanish versions of the documents.
+    """
     from sdmx.model.common import Code, Codelist
 
     cl = Codelist(
@@ -76,15 +87,182 @@ Emissions from fuel sold to any air or marine vessel engaged in international tr
     return cl
 
 
-def gen_structures() -> None:
-    """Create or update IPCC-maintained structural metadata.
+def gen_cs_ch3(**kwargs) -> "sdmx.model.common.ConceptScheme":
+    """Generate a scheme of concepts included in equations in Chapter 3.
 
-    The structures have URNs like ``TDCI:CS_IPCC_{NAME}(0.1)``
+    The generated code list's URN ends with
+    ``ConceptScheme=TDCI:CS_IPCC_2006_V2_CH3(…)``.
+
+    .. todo:: Include concepts used as table dimensions.
+
+    .. todo:: Include internationalized texts (names, descriptions) from the Arabic,
+       Chinese, French, Russian, and/or Spanish versions of the documents.
     """
-    from transport_data import STORE, org
+    from sdmx.model.common import Annotation, Concept, ConceptScheme
 
-    def _make_id(value: str) -> str:
-        return f"{get_agency().id}_{value}"
+    cs = ConceptScheme(
+        id="CS_IPCC_2006_V2_CH3", name="Concepts appearing in equations", **kwargs
+    )
+
+    equation, page = "", ""
+
+    def _c(id_, name=None, units=None, description=None):
+        c = Concept(
+            id=id_,
+            name=name,
+            description=f"First appears in Equation {equation} on p.{page}",
+        )
+
+        if units:
+            c.annotations.append(Annotation(id="preferred-units", text=units))
+        cs.append(c)
+
+    # §3.2 Road transportation
+
+    equation, page = "3.2.1", "3.12"
+    _c(
+        "EMI 1",
+        "Emissions",
+        "kg",
+        """Variously "Emissions of CO₂" (Eq. 3.2.1), or of varying species (Eq. 3.2.3, 3.2.5)""",
+    )
+    _c("Fuel 1", "Fuel sold", "TJ")
+    _c("EF 1", "Emission factor", "kg/TJ")
+    _c(
+        "a",
+        "Type of fuel (e.g. petrol, diesel, natural gas, lpg)",
+        None,
+        "In Eq 3.2.6, 'j' is used for the same concept.",
+    )
+
+    equation, page = "3.2.2", "3.12"
+    _c(
+        "EMI 2",
+        "CO₂ Emissions from urea-based additive in catalytic converters",
+        "Gg CO₂",
+    )
+    _c(
+        "Activity",
+        "amount of urea-based additive consumed for use in catalytic converters",
+        "Gg",
+    )
+    _c(
+        "Purity",
+        "the mass fraction (=percentage divided by 100) of urea in the urea-based additive",
+    )
+
+    # Eq. 3.2.3 —same concepts as 3.2.1
+
+    equation, page = "3.2.4", "3.13"
+    _c(
+        "Fuel 2",
+        "fuel consumed (as represented by fuel sold) for a given mobile source activity",
+        "TJ",
+    )
+    _c(
+        "b",
+        "vehicle type",
+        None,
+        "In Eq 3.2.6, 'i' is used for the same concept (e.g., car, bus)",
+    )
+    _c(
+        "c",
+        "emission control technology (such as uncontrolled, catalytic converter, etc.)",
+    )
+
+    equation, page = "3.2.5", "3.15"
+    _c("EF 2", "emission factor", "kg / km")
+    _c(
+        "Distance 1",
+        "distance travelled during thermally stabilized engine operation phase for a given mobile source activity",
+        "km",
+    )
+    _c("C", "emissions during warm-up phase (cold start)", "kg")
+    _c(
+        "d",
+        "operating conditions (e.g. urban or rural road type, climate, or other environmental factors)",
+    )
+
+    equation, page = "3.2.6", "3.26"
+    _c(
+        "Estimated fuel",
+        "total estimated fuel use estimated from distance travelled (VKT) data",
+        "litre",
+    )
+    _c("Vehicles", "number of vehicles of type i and using fuel j on road type t")
+    _c(
+        "Distance 2",
+        "annual kilometres travelled per vehicle of type i and using fuel j on road type t",
+        "km",
+    )
+    _c("t", "type of road (e.g., urban, rural)")
+
+    # §3.3 Off-road transportation
+    # Eq. 3.3.1 —no additional concepts
+    # Eq. 3.3.2 —no additional concepts
+
+    equation, page = "3.3.3", "3.34"
+    _c(
+        "N",
+        "source population",
+        None,
+        """In Eq. 3.4.3 this is given as 'number of locomotives of type i".""",
+    )
+    # Ditto below, all used in Eq. 3.4.3
+    _c("H", "annual hours of use of vehicle i", "hour")
+    _c("P", "average rated power of vehicle i", "kW")
+    _c("LF", "typical load factor of vehicle i (fraction between 0 and 1)")
+    _c("EF 3", "average emission factor for use of fuel j in vehicle i", "kg / kWh")
+
+    # Eq. 3.3.4 —no additional concepts
+
+    # §3.4 Railways
+    # Eq. 3.4.1 —no additional concepts
+    # Eq. 3.4.2 —no additional concepts
+
+    equation, page = "3.4.3", "3.42"
+    _c("i", "locomotive type and journey type")
+
+    equation, page = "3.4.4", "3.43"
+    _c("EF 4", "engine specific emission factor for locomotive of type i", "kg/TJ")
+    _c("PWF", "pollutant weighting factor for locomotive of type i", "dimensionless")
+    _c("EF 5", "default emission factor for diesel (applies to CH₄, N₂O)", "kg/TJ")
+
+    # §3.6 Civil Aviation
+    # Eq. 3.6.1 —no additional concepts
+
+    equation, page = "3.6.2", "3.59"
+    _c(
+        "Emissions.LTO",
+        "",
+        None,
+        """'LTO' is defined on p.3.56 as "Landing/Take-Off cycle".""",
+    )
+    _c(
+        "Emissions.Cruise",
+        "",
+        None,
+        """'Cruise' is defined on p.3.56 in contrast with 'LTO'.""",
+    )
+
+    equation, page = "3.6.3", "3.59"
+    _c("Number of LTOs")
+    _c("EF.LTO")
+
+    equation, page = "3.6.4", "3.59"
+    _c("Fuel consumption.LTO")
+    _c("Fuel consumption per LTO")
+
+    equation, page = "3.6.5", "3.59"
+    _c("Total Fuel Consumption")
+    _c("EF.Cruise")
+
+    return cs
+
+
+def gen_structures() -> None:
+    """Create or update IPCC-maintained structural metadata."""
+    from transport_data import STORE, org
 
     ma_args = dict(
         maintainer=org.get_agency()[0],
@@ -94,3 +272,4 @@ def gen_structures() -> None:
     )
 
     STORE.setdefault(gen_cl_T311(**ma_args))
+    STORE.setdefault(gen_cs_ch3(**ma_args))
