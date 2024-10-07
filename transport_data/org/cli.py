f8a83271c34cc1bde9338cf31ab4f118d09f6745
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-10-07 11:48:55 +0200

    Add a catch-all command for TUEWAS outputs

--- transport_data/org/cli.py
+++ transport_data/org/cli.py
@@ -78,9 +78,52 @@ def summarize(path_in: "pathlib.Path", path_out: Optional["pathlib.Path"], ref_a
     elif 1 < len(ref_areas):
         # Report for multiple REF_AREA
         if path_out is None:
-            path_out = pathlib.Path.cwd().joinpath("all.html")
+            path_out = pathlib.Path.cwd().joinpath("all.{html,odt}")
             print(f"Write to {path_out}")
-        report.MetadataSet2HTML(mds, ref_area=ref_areas).write_file(path_out)
+        report.MetadataSet0ODT(mds).write_file(path_out.with_suffix(".odt"))
+        report.MetadataSet2HTML(mds, ref_area=ref_areas).write_file(
+            path_out.with_suffix(".html")
+        )
+
+
+@main.command("tuewas")
+@click.argument(
+    "path_in", type=click.Path(exists=True, dir_okay=False, path_type=pathlib.Path)
+)
+def _tuewas_all(path_in):
+    """Generate all outputs for TUEWAS."""
+    from zipfile import ZipFile
+
+    from .metadata import report
+    from .metadata.spreadsheet import read_workbook
+
+    ref_areas = "CN ID IN PH TH VN".split()
+
+    mds, _ = read_workbook(path_in.resolve())
+
+    dir_out = pathlib.Path.cwd().joinpath("output")
+    path_out = []
+
+    for ref_area in ref_areas:
+        path_out.append(dir_out.joinpath(ref_area, "Summary.odt"))
+        path_out[-1].parent.mkdir(parents=True, exist_ok=True)
+        report.MetadataSet1ODT(mds, ref_area=ref_areas[0]).write_file(path_out[-1])
+        print(f"Wrote {path_out[-1]}")
+
+    path_out.append(dir_out.joinpath("Metadata summary.odt"))
+    report.MetadataSet0ODT(mds).write_file(path_out[-1])
+    print(f"Wrote {path_out[-1]}")
+
+    path_out.append(dir_out.joinpath("Metadata summary table.html"))
+    report.MetadataSet2HTML(mds, ref_area=ref_areas).write_file(path_out[-1])
+    print(f"Wrote {path_out[-1]}")
+
+    path_zip = dir_out.joinpath("all.zip")
+    with ZipFile(path_zip, mode="w") as zf:
+        for p in path_out:
+            zf.write(p, str(p.relative_to(dir_out)))
+
+    print(f"Wrote {path_zip}")
 
 
 @main.command("template")
