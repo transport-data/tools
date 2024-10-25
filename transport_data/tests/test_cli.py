cebfac600be27142bcc1dfcb5f0113ac37e455d0
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-10-25 14:57:26 +0200

    Test "tdc check" command

--- transport_data/tests/test_cli.py
+++ transport_data/tests/test_cli.py
@@ -1,7 +1,6 @@
-import pytest
-from click.testing import CliRunner
+import re
 
-from transport_data.cli import main
+import pytest
 
 
 @pytest.mark.parametrize(
@@ -10,6 +9,7 @@ from transport_data.cli import main
         ("--help",),
         ("adb", "--help"),
         ("adb", "fetch", "--all"),
+        ("check", "--help"),
         ("config", "--help"),
         ("estat", "--help"),
         ("estat", "fetch", "--help"),
@@ -23,6 +23,35 @@ from transport_data.cli import main
         ("store", "--help"),
     ),
 )
-def test_cli(command):
-    runner = CliRunner()
-    runner.invoke(main, command)
+def test_cli(tdc_cli, command):
+    tdc_cli.invoke(command)
+
+
+def test_check(tdc_cli, test_data_path):
+    result = tdc_cli.invoke(
+        [
+            "check",
+            "--sheets=sheet_2",
+            "--structure=dataflow",
+            "--structure-id=FOO",
+            "--action=I",
+            str(test_data_path.joinpath("read-csv-2.xlsx")),
+        ]
+    )
+
+    # Command runs without error
+    assert 0 == result.exit_code, result.output
+
+    # Data was located, converted to CSV, read, and summary displayed
+    assert re.fullmatch(
+        r"""
+File: .*read-csv-2.xlsx
+Sheet: sheet_2
+
+1 data set\(s\) in data flow Dataflow=TDCI:EMBER_001\(1.0.0\)
+
+Data set 0: action=ActionType.information
+120 observations
+""",
+        result.output,
+    )
