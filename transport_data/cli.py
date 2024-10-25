7cfc974f66daf5c097abb5788a83432096973105
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-10-25 14:43:59 +0200

    Handle .xlsx files in "tdc check"

--- transport_data/cli.py
+++ transport_data/cli.py
@@ -35,15 +35,16 @@ for name in MODULES_WITH_CLI:
     main.add_command(getattr(module, "main"))
 
 
-@main.command("check")
+@main.command()
 @click.argument(
     "path", metavar="FILE", type=click.Path(exists=True, dir_okay=False, path_type=Path)
 )
-@click.option("-v", "--verbose", count=True, help="Increase verbosity")
+@click.option("--sheets", help="Sheet(s) in .xlsx FILE to check.")
+@click.option("-v", "--verbose", count=True, help="Increase verbosity.")
 @click.option("--structure", help="Value for STRUCTURE field.")
 @click.option("--structure-id", "structure_id", help="Value for STRUCTURE_ID field.")
 @click.option("--action", help="Value for ACTION field.")
-def check_csv(path: Path, verbose, **adapt):
+def check(path: Path, sheets, verbose, **adapt):  # noqa: C901
     """Check that FILE can be read as SDMX-CSV."""
     import sdmx
     import sdmx.urn
@@ -51,41 +52,72 @@ def check_csv(path: Path, verbose, **adapt):
     from transport_data.testing import ember_dfd
     from transport_data.util.sdmx import read_csv
 
-    # Check for a supported file extension
-    if not path.suffix == ".csv":
+    # Sequence of (label, path) for CSV files to be processed
+    label_path = []
+
+    if path.suffix == ".csv":
+        label_path.append((f"File: {path}", path))
+    elif path.suffix == ".xlsx":
+        import pandas as pd
+        from platformdirs import user_cache_path
+
+        # Create a cache directory
+        cache_dir = user_cache_path("transport-data").joinpath("check")
+        cache_dir.mkdir(parents=True, exist_ok=True)
+
+        # Explode Excel file into one CSV file per sheet
+        ef = pd.ExcelFile(path)
+        _sheets = set(sheets.split(",")) if sheets else set(ef.sheet_names)
+        for sheet_name in filter(_sheets.__contains__, ef.sheet_names):
+            # Construct a temporary path
+            label_path.append(
+                (
+                    f"File: {path}\nSheet: {sheet_name}",
+                    cache_dir.joinpath(f"{path.stem}_xlsx_{sheet_name}.csv"),
+                )
+            )
+            # Read the sheet from the ExcelFile and write to a CSV file
+            pd.read_excel(ef, sheet_name).to_csv(label_path[-1][1], index=False)
+        ef.close()
+    else:
         raise click.UsageError(f"Unsupported file extension: {path.suffix!r}")
 
-    print(f"File: {path}")
-
-    # TEMPORARY Use a test utility function to get a DFD that describes the file
+    # TEMPORARY Use a test utility function to get a DFD that describes the file(s)
     structure = ember_dfd()
     # TODO Look up a data structure to use in STORE
 
-    # Read the file into an SDMX data message
-    try:
-        dm = read_csv(path, structure, adapt)
-    except Exception as e:
-        message = [f"read failed with\n{type(e).__name__}: {' '.join(e.args)}"]
+    for label, p in label_path:
+        print(f"\n{label}")
 
-        if "line 1" in e.args[0]:
-            message.append(
-                "Hint: try giving --structure= or --structure-id argument(s) to adapt"
-                " to SDMX-CSV."
-            )
-        elif isinstance(e, KeyError):
-            message.append("Hint: try giving --action argument to adapt to SDMX-CSV.")
+        # Read the file into an SDMX data message
+        try:
+            dm = read_csv(p, structure, adapt)
+        except Exception as e:
+            message = [f"read failed with\n{type(e).__name__}: {' '.join(e.args)}"]
 
-        print("")
-        raise click.ClickException("\n\n".join(message))
+            if "line 1" in e.args[0]:
+                message.append(
+                    "Hint: try giving --structure= or --structure-id argument(s) to "
+                    "adapt to SDMX-CSV."
+                )
+            elif isinstance(e, KeyError):
+                message.append(
+                    "Hint: try giving --action argument to adapt to SDMX-CSV."
+                )
 
-    dfd_urn = sdmx.urn.shorten(sdmx.urn.make(dm.dataflow))
-    print(f"\n{len(dm.data)} data set(s) in data flow {dfd_urn!s}")
+            print("")
+            raise click.ClickException("\n\n".join(message))
 
-    for i, ds in enumerate(dm.data):
-        print(f"\nData set {i}: action={ds.action}")
-        if verbose == 0:
-            print(f"{len(ds)} observations")
-        elif verbose == 1:
-            print(sdmx.to_pandas(ds))
-        else:
-            print(sdmx.to_pandas(ds).to_string())
+        dfd_urn = sdmx.urn.shorten(sdmx.urn.make(dm.dataflow))
+        print(f"\n{len(dm.data)} data set(s) in data flow {dfd_urn!s}")
+
+        for i, ds in enumerate(dm.data):
+            print(f"\nData set {i}: action={ds.action}")
+
+            # Show information about the contents, according to verbosity
+            if verbose == 0:
+                print(f"{len(ds)} observations")
+            elif verbose == 1:
+                print(sdmx.to_pandas(ds))
+            else:
+                print(sdmx.to_pandas(ds).to_string())
