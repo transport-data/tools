327c543bb4c02fd603a9e77827abbb37a75a0e9a
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-12-13 11:21:25 +0100

    Convert .ckan.Package to/from SDMX; test

--- transport_data/tests/org/test_ckan.py
+++ transport_data/tests/org/test_ckan.py
@@ -1,7 +1,20 @@
 import pytest
 from requests.exceptions import SSLError
 
-from transport_data.org.ckan import DEV, PROD, STAGING
+from transport_data.org.ckan import (
+    DEV,
+    PROD,
+    STAGING,
+    ckan_package_to_mdr,
+    mdr_to_ckan_package,
+)
+from transport_data.util.ckan import Package
+
+
+@pytest.fixture
+def package(test_data_path) -> Package:
+    """A :class:`.Package` from a test specimen."""
+    return Package.from_file(test_data_path.joinpath("ckan", "package.json"))
 
 
 @pytest.mark.parametrize(
@@ -24,3 +37,34 @@ def test_main(instance, N_exp: int) -> None:
     assert p is result[0]  # Same object instance returned
 
     assert "dataset" == p.type
+
+
+def test_ckan_package_to_mdr(package: Package) -> None:
+    """:func:`.ckan_package_to_mdr` works as expected."""
+    from transport_data.org.metadata import _get
+
+    # Function runs
+    mdr = ckan_package_to_mdr(package)
+
+    # Resulting MetadataReport has the expected number of attributes
+    assert 48 == len(mdr.metadata)
+
+    # Structured attributes are stored as their string representations
+    assert repr(["cars", "private-cars", "truck", "bus"]) == _get(mdr, "modes")
+    # Python types are stored as their representations
+    assert "False" == _get(mdr, "is_archived")
+    assert "True" == _get(mdr, "isopen")
+    assert "None" == _get(mdr, "author")
+
+
+def test_mdr_to_ckan_package(package: Package) -> None:
+    """:func:`.mdr_to_ckan_package` works as expected."""
+
+    # Convert to a MetadataReport
+    mdr = ckan_package_to_mdr(package)
+
+    # Convert back to a Package instance
+    p = mdr_to_ckan_package(mdr)
+
+    if package != p:
+        assert package.__dict__ == p.__dict__  # `pytest -vv` shows the rich comparison
