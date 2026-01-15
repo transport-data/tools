f8652b6fba3955da154f08f95ae193ff5237d81e
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2026-01-15 23:28:03 +0100

    Work around ckan/ckanapi#218

--- transport_data/__init__.py
+++ transport_data/__init__.py
@@ -1,9 +1,12 @@
 import logging
 import sys
+from warnings import filterwarnings
 
 from .config import Config
 from .store import UnionStore
 from .util.pluggy import register_internal
+
+filterwarnings("ignore", "pkg_resources is deprecated", UserWarning, "ckanapi.version")
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
